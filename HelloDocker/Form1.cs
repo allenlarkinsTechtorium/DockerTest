a1a3a2299c84fa6ba4605790e88972275c6eb16b
Allen Larkins <allenlarkins@gmail.com>
2018-02-26 15:02:25 +1300

    Initial commit

--- HelloDocker/Form1.cs
+++ HelloDocker/Form1.cs
@@ -16,5 +16,7 @@ namespace HelloDocker
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e) => MessageBox.Show("Docked!!");
     }
 }
